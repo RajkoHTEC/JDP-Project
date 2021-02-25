03ea67b0ea48292f3404633ad828264ff5bfa06c
Rajko <rmilanrajkovic@gmail.com>
2021-02-25 15:19:27 +0100

    Testing launch flags

---
+++ app/src/main/java/com/htec/jdp/AActivity.kt
@@ -0,0 +1,10 @@
+package com.htec.jdp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
